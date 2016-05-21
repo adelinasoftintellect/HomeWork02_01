fd1cc8cff85f2447da4ce47b5b052b49f6b60504
Adi <adelina_yordanova11@abv.bg>
2016-05-21 13:08:53 +0300

    Homework02_01

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		float length = 3.456f;
+		System.out.print("Length ");
+		System.out.println(length);
+		float width = 6.789f;
+		System.out.print("Width ");
+		System.out.println(width);
+		float S = length * width;
+		System.out.println("S = (a*b) " + S);
+
+	}
+
+}
